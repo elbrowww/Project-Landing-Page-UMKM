37da4d8aed76e2b1c80541da2428d39b78fbe38d
elbrowww <angellodewangga784@gmail.com>
2025-11-02 22:51:24 +0700

    update Koneksi by EL

--- config/koneksi.php
+++ config/koneksi.php
@@ -2,16 +2,12 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "database_bu_mon"; // nama database sesuai permintaan
+$db   = "database_bu_mon"; 
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
-
-// Jika juga butuh PDO
-$pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
