e77218937c51c199135bc8769f3d95e25fe1110f
nilaaurelia4 <nilaagustinprima@gmail.com>
2025-11-04 19:32:55 +0700

    perbaikan php by nila

--- index.php
+++ index.php
@@ -90,14 +90,13 @@
     <p class="text-center text-gray-600 mb-12 fade-in">Beragam menu tersedia untuk berbagai acara</p>
 
     <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-6">
-      
-      include "koneksi landing page.php";
+      <?php
+      include "koneksi.php";
       $query = mysqli_query($koneksi, "SELECT nama_menu, deskripsi, harga_menu, gambar FROM menu");
-      while ($data = mysqli_fetch_array($query)) <?php {
-      
+      while ($data = mysqli_fetch_array($query)) {
+      ?>
         <div class="menu-item fade-in bg-white rounded-2xl overflow-hidden shadow-lg card-hover">
-          <img src="/../asset/uploads/<?php echo $data['gambar']; ?>" alt="<?php echo $data['nama_menu']; ?>" class="w-full h-48 object-cover">
-
+          <img src="asset/img/<?php echo $data['gambar']; ?>" alt="<?php echo $data['nama_menu']; ?>" class="w-full h-48 object-cover">
           <div class="p-6">
             <h3 class="text-xl font-bold mb-2"><?php echo $data['nama_menu']; ?></h3>
             <p class="text-gray-600 mb-4"><?php echo $data['deskripsi']; ?></p>
@@ -107,7 +106,7 @@
             </a>
           </div>
         </div>
-       } ?>
+      <?php } ?>
     </div>
   </div>
 </section>
