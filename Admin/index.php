ea66aa7c5e1a372c8c090cda2f0c93fdc29e2a38
cintyaazw40-lang <cintyaazw40@gmail.com>
2025-11-05 10:41:32 +0700

    ubah css by cintya

--- Admin/index.php
+++ Admin/index.php
@@ -16,7 +16,7 @@ include '../config/CRUD.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard Admin | Bu Mon</title>
-  <link rel="icon" href="../asset/img/logo.png" type="image/x-icon">
+   <link rel="icon" href="../asset/img/logo.png" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
