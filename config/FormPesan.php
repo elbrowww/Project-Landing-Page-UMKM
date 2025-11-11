a3567836ce6a0606358d106fb91b8a88b2dd64c1
nilaaurelia4 <nilaagustinprima@gmail.com>
2025-11-11 18:46:49 +0700

    perbaikan FormPesan by nila

--- config/FormPesan.php
+++ config/FormPesan.php
@@ -2,20 +2,23 @@
 include '../Project-Landing-Page-UMKM/config/koneksi.php';
 
 if (isset($_POST['simpan'])) {
-    $nama   = ['nama_pelanggan'];
-    $email  = ['email_pelanggan'];
-    $notelp = ['notelp_pelanggan'];
-    $pesan  = ['isi_pesan'];
+    $nama   = $_POST['nama_pelanggan'];
+    $email  = $_POST['email_pelanggan'];
+    $notelp = $_POST['notelp_pelanggan'];
+    $pesan  = $_POST['isi_pesan'];
 
-   
-     if ($id == "") {
-        // CREATE
-        $stmt = $koneksi->prepare("INSERT INTO pesan_pelanggan (nama_pelanggan, email_pelanggan, notelp_pelanggan, isi_pesan) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param("sidss", $nama, $email, $notelp, $pesan);
-     }
+    // CREATE
+    $stmt = $koneksi->prepare("INSERT INTO pesan_pelanggan (nama_pelanggan, email_pelanggan, notelp_pelanggan, isi_pesan) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $nama, $email, $notelp, $pesan);
 
-    $stmt->execute();
-    header("Location: index.php");
-    exit;
+    if ($stmt->execute()) {
+        // Jika Berhasil
+        echo "<script>alert('Pesan berhasil dikirim!'); window.location.href='index.php#kontak';</script>";
+    } else {
+        // Jika Gagal
+        echo "<script>alert('Terjadi kesalahan saat mengirim pesan.');</script>";
+    }
+
+    $stmt->close();
 }
 ?>
