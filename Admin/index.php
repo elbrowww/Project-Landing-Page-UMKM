1b369702077da886e5cde2b75d761e35d3ca4a56
cintyaazw40-lang <cintyaazw40@gmail.com>
2025-11-11 08:07:16 +0700

    css admin(masih separu) by cintya

--- Admin/index.php
+++ Admin/index.php
@@ -21,7 +21,7 @@ include '../config/CRUD.php';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard Admin | Bu Mon</title>
    <link rel="icon" href="../asset/img/logo.png" type="image/x-icon">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../asset/css/admin.css" rel="stylesheet">
 </head>
 <body class="bg-light">
 
