4ef0d508d1282c5fbcbaf0b585e1649cf3298010
etnis13 <epradnaa@gmail.com>
2025-11-16 18:21:22 +0700

    hps

--- index.php
+++ index.php
@@ -48,13 +48,7 @@
       </svg>
     </button>
   </div>
-      </div>
-      <button id="menuBtn" class="md:hidden text-white" aria-label="Buka menu">
-        <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
-        </svg>
-      </button>
-    </div>
+      
     <div id="mobileMenu" class="hidden md:hidden bg-purple/90 backdrop-blur-sm px-4 py-4">
       <a class="block py-2 text-gray-700" href="#home">Beranda</a>
       <a class="block py-2 text-gray-700" href="#menu">Menu</a>
