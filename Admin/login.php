0bf37825986565c865ba7173b8c3a50e2ed71f5a
elbrowww <angellodewangga784@gmail.com>
2025-11-25 19:44:29 +0700

    login to halaman utama by EL

--- Admin/login.php
+++ Admin/login.php
@@ -44,7 +44,7 @@ if (isset($_POST['login'])) {
                     <img src="../asset/img/logo-nobg.png" alt="Dapur Bu Mon Logo" class="logo-img">
                     <div class="logo-text">
                         <h1>Dapur Bu Mon</h1>
-                        <p>Admin Dashboard</p>
+                        <p><a href="../index.php" class="btn-back">Admin Dashboard</a></p>
                     </div>
                 </div>
             </div>
@@ -61,7 +61,6 @@ if (isset($_POST['login'])) {
                 </div>
    
                 <button type="submit" class="btn-login" name="login">Masuk</button>
-                
 
             </form>
         </div>
